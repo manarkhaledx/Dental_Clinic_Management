7610dfe0ef7054d07acf34a4aa717ebbef55b9b9
Mark Mamdouh <mark.mamdouh67@yahoo.com>
2023-12-07 22:11:49 +0200

    add login form

--- Dental-Clinic_Management/DashBoard.Designer.cs
+++ Dental-Clinic_Management/DashBoard.Designer.cs
@@ -96,7 +96,7 @@
             this.panel4.Controls.Add(this.pictureBox3);
             this.panel4.Controls.Add(this.label10);
             this.panel4.Controls.Add(this.a);
-            this.panel4.Location = new System.Drawing.Point(620, 644);
+            this.panel4.Location = new System.Drawing.Point(514, 644);
             this.panel4.Name = "panel4";
             this.panel4.Size = new System.Drawing.Size(603, 176);
             this.panel4.TabIndex = 6;
@@ -118,7 +118,7 @@
             this.label10.Font = new System.Drawing.Font("Palatino Linotype", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label10.Location = new System.Drawing.Point(484, 133);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(26, 31);
+            this.label10.Size = new System.Drawing.Size(22, 26);
             this.label10.TabIndex = 5;
             this.label10.Text = "0";
             this.label10.Click += new System.EventHandler(this.label10_Click);
@@ -129,7 +129,7 @@
             this.a.Font = new System.Drawing.Font("Palatino Linotype", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.a.Location = new System.Drawing.Point(294, 28);
             this.a.Name = "a";
-            this.a.Size = new System.Drawing.Size(363, 39);
+            this.a.Size = new System.Drawing.Size(246, 26);
             this.a.TabIndex = 1;
             this.a.Text = "Completed Appointments";
             this.a.Click += new System.EventHandler(this.a_Click);
@@ -141,7 +141,7 @@
             this.panel1.Controls.Add(this.pictureBox4);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.label2);
-            this.panel1.Location = new System.Drawing.Point(1442, 644);
+            this.panel1.Location = new System.Drawing.Point(1336, 644);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(603, 176);
             this.panel1.TabIndex = 12;
@@ -163,7 +163,7 @@
             this.label1.Font = new System.Drawing.Font("Palatino Linotype", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(484, 133);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(26, 31);
+            this.label1.Size = new System.Drawing.Size(22, 26);
             this.label1.TabIndex = 5;
             this.label1.Text = "0";
             // 
@@ -173,7 +173,7 @@
             this.label2.Font = new System.Drawing.Font("Palatino Linotype", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(449, 9);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(88, 31);
+            this.label2.Size = new System.Drawing.Size(75, 26);
             this.label2.TabIndex = 1;
             this.label2.Text = "Patient";
             this.label2.Click += new System.EventHandler(this.label2_Click);
@@ -185,7 +185,7 @@
             this.appointmentsPanel.Controls.Add(this.pictureBox5);
             this.appointmentsPanel.Controls.Add(this.label3);
             this.appointmentsPanel.Controls.Add(this.label4);
-            this.appointmentsPanel.Location = new System.Drawing.Point(620, 162);
+            this.appointmentsPanel.Location = new System.Drawing.Point(514, 162);
             this.appointmentsPanel.Name = "appointmentsPanel";
             this.appointmentsPanel.Size = new System.Drawing.Size(603, 176);
             this.appointmentsPanel.TabIndex = 13;
@@ -207,7 +207,7 @@
             this.label3.Font = new System.Drawing.Font("Palatino Linotype", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.Location = new System.Drawing.Point(484, 133);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(26, 31);
+            this.label3.Size = new System.Drawing.Size(22, 26);
             this.label3.TabIndex = 5;
             this.label3.Text = "0";
             // 
@@ -217,7 +217,7 @@
             this.label4.Font = new System.Drawing.Font("Palatino Linotype", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.Location = new System.Drawing.Point(399, 9);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(173, 31);
+            this.label4.Size = new System.Drawing.Size(147, 26);
             this.label4.TabIndex = 1;
             this.label4.Text = "Appointments ";
             // 
@@ -228,7 +228,7 @@
             this.panel3.Controls.Add(this.pictureBox6);
             this.panel3.Controls.Add(this.label5);
             this.panel3.Controls.Add(this.label6);
-            this.panel3.Location = new System.Drawing.Point(1442, 162);
+            this.panel3.Location = new System.Drawing.Point(1336, 162);
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(603, 176);
             this.panel3.TabIndex = 14;
@@ -250,7 +250,7 @@
             this.label5.Font = new System.Drawing.Font("Palatino Linotype", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.Location = new System.Drawing.Point(484, 133);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(26, 31);
+            this.label5.Size = new System.Drawing.Size(22, 26);
             this.label5.TabIndex = 5;
             this.label5.Text = "0";
             // 
@@ -260,7 +260,7 @@
             this.label6.Font = new System.Drawing.Font("Palatino Linotype", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.Location = new System.Drawing.Point(399, 9);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(173, 31);
+            this.label6.Size = new System.Drawing.Size(147, 26);
             this.label6.TabIndex = 1;
             this.label6.Text = "Appointments ";
             // 
@@ -270,7 +270,7 @@
             this.panel5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panel5.Controls.Add(this.pictureBox7);
             this.panel5.Controls.Add(this.label7);
-            this.panel5.Location = new System.Drawing.Point(620, 402);
+            this.panel5.Location = new System.Drawing.Point(514, 402);
             this.panel5.Name = "panel5";
             this.panel5.Size = new System.Drawing.Size(603, 176);
             this.panel5.TabIndex = 15;
@@ -292,7 +292,7 @@
             this.label7.Font = new System.Drawing.Font("Palatino Linotype", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.Location = new System.Drawing.Point(484, 133);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(26, 31);
+            this.label7.Size = new System.Drawing.Size(22, 26);
             this.label7.TabIndex = 5;
             this.label7.Text = "0";
             // 
@@ -302,7 +302,7 @@
             this.label8.Font = new System.Drawing.Font("Palatino Linotype", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label8.Location = new System.Drawing.Point(1061, 424);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(92, 31);
+            this.label8.Size = new System.Drawing.Size(78, 26);
             this.label8.TabIndex = 1;
             this.label8.Text = "Dentist";
             // 
@@ -312,7 +312,7 @@
             this.panel6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panel6.Controls.Add(this.pictureBox8);
             this.panel6.Controls.Add(this.label9);
-            this.panel6.Location = new System.Drawing.Point(1442, 402);
+            this.panel6.Location = new System.Drawing.Point(1336, 402);
             this.panel6.Name = "panel6";
             this.panel6.Size = new System.Drawing.Size(603, 176);
             this.panel6.TabIndex = 16;
@@ -334,7 +334,7 @@
             this.label9.Font = new System.Drawing.Font("Palatino Linotype", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label9.Location = new System.Drawing.Point(484, 133);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(26, 31);
+            this.label9.Size = new System.Drawing.Size(22, 26);
             this.label9.TabIndex = 5;
             this.label9.Text = "0";
             // 
@@ -344,7 +344,7 @@
             this.label11.Font = new System.Drawing.Font("Palatino Linotype", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label11.Location = new System.Drawing.Point(1881, 424);
             this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(94, 31);
+            this.label11.Size = new System.Drawing.Size(80, 26);
             this.label11.TabIndex = 1;
             this.label11.Text = "Billings";
             // 
@@ -573,7 +573,7 @@
             this.pictureBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBox2.BackgroundImage = global::Dental_Clinic_Management.Properties.Resources.Minimize;
             this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox2.Location = new System.Drawing.Point(2116, 10);
+            this.pictureBox2.Location = new System.Drawing.Point(1903, 10);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(48, 45);
             this.pictureBox2.TabIndex = 11;
@@ -585,7 +585,7 @@
             this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBox1.BackgroundImage = global::Dental_Clinic_Management.Properties.Resources.Exit;
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox1.Location = new System.Drawing.Point(2169, 10);
+            this.pictureBox1.Location = new System.Drawing.Point(1956, 10);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(41, 45);
             this.pictureBox1.TabIndex = 10;
@@ -598,7 +598,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.ClientSize = new System.Drawing.Size(2153, 1098);
+            this.ClientSize = new System.Drawing.Size(1940, 1098);
             this.ControlBox = false;
             this.Controls.Add(this.button10);
             this.Controls.Add(this.panel7);
---
+++ Dental-Clinic_Management/Login.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace Dental_Clinic_Management
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Location = new System.Drawing.Point(30, 277);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(469, 2);
+            this.panel1.TabIndex = 0;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(533, 343);
+            this.ControlBox = false;
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+    }
+}
