0d1ae07a66eb86795114fd556283e5e194326624
Mark Mamdouh <mark.mamdouh67@yahoo.com>
2023-12-09 00:14:22 +0200

    merged

--- Dental-Clinic_Management/addPrescriptionForm.Designer.cs
+++ Dental-Clinic_Management/addPrescriptionForm.Designer.cs
@@ -28,12 +28,75 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.allergiesTextBox = new System.Windows.Forms.TextBox();
+            this.allergiesLabel = new System.Windows.Forms.Label();
+            this.addRecordLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // saveButton
+            // 
+            this.saveButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(134)))), ((int)(((byte)(167)))), ((int)(((byte)(137)))));
+            this.saveButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.saveButton.FlatAppearance.BorderSize = 0;
+            this.saveButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.saveButton.Location = new System.Drawing.Point(624, 731);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(136, 56);
+            this.saveButton.TabIndex = 36;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = false;
+            // 
+            // allergiesTextBox
+            // 
+            this.allergiesTextBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(9)))), ((int)(((byte)(111)))), ((int)(((byte)(24)))));
+            this.allergiesTextBox.Location = new System.Drawing.Point(175, 281);
+            this.allergiesTextBox.Multiline = true;
+            this.allergiesTextBox.Name = "allergiesTextBox";
+            this.allergiesTextBox.Size = new System.Drawing.Size(515, 421);
+            this.allergiesTextBox.TabIndex = 34;
+            // 
+            // allergiesLabel
+            // 
+            this.allergiesLabel.AutoSize = true;
+            this.allergiesLabel.Font = new System.Drawing.Font("Microsoft Uighur", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.allergiesLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(9)))), ((int)(((byte)(111)))), ((int)(((byte)(24)))));
+            this.allergiesLabel.Location = new System.Drawing.Point(359, 205);
+            this.allergiesLabel.Name = "allergiesLabel";
+            this.allergiesLabel.Size = new System.Drawing.Size(158, 45);
+            this.allergiesLabel.TabIndex = 32;
+            this.allergiesLabel.Text = "Prescription";
+            // 
+            // addRecordLabel
+            // 
+            this.addRecordLabel.AutoSize = true;
+            this.addRecordLabel.Font = new System.Drawing.Font("Times New Roman", 28.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addRecordLabel.Location = new System.Drawing.Point(63, 38);
+            this.addRecordLabel.Name = "addRecordLabel";
+            this.addRecordLabel.Size = new System.Drawing.Size(372, 52);
+            this.addRecordLabel.TabIndex = 29;
+            this.addRecordLabel.Text = "Add Presecription";
+            // 
+            // addPrescriptionForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(782, 835);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.allergiesTextBox);
+            this.Controls.Add(this.allergiesLabel);
+            this.Controls.Add(this.addRecordLabel);
+            this.Name = "addPrescriptionForm";
             this.Text = "addPrescriptionForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.TextBox allergiesTextBox;
+        private System.Windows.Forms.Label allergiesLabel;
+        private System.Windows.Forms.Label addRecordLabel;
     }
 }
--- Dental-Clinic_Management/showAppointmentsOfTheDayForm.Designer.cs
+++ Dental-Clinic_Management/showAppointmentsOfTheDayForm.Designer.cs
@@ -28,12 +28,98 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.billingDataGridView = new System.Windows.Forms.DataGridView();
+            this.pickDateLabel = new System.Windows.Forms.Label();
+            this.appointmentID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.patientName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.patientPhone = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.filterButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.billingDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // billingDataGridView
+            // 
+            this.billingDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.billingDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.appointmentID,
+            this.patientName,
+            this.patientPhone});
+            this.billingDataGridView.Location = new System.Drawing.Point(51, 209);
+            this.billingDataGridView.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.billingDataGridView.Name = "billingDataGridView";
+            this.billingDataGridView.RowHeadersWidth = 62;
+            this.billingDataGridView.RowTemplate.Height = 28;
+            this.billingDataGridView.Size = new System.Drawing.Size(1170, 574);
+            this.billingDataGridView.TabIndex = 63;
+            // 
+            // pickDateLabel
+            // 
+            this.pickDateLabel.AutoSize = true;
+            this.pickDateLabel.Font = new System.Drawing.Font("Times New Roman", 28.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pickDateLabel.Location = new System.Drawing.Point(519, 56);
+            this.pickDateLabel.Name = "pickDateLabel";
+            this.pickDateLabel.Size = new System.Drawing.Size(497, 52);
+            this.pickDateLabel.TabIndex = 62;
+            this.pickDateLabel.Text = "Appointments of the day";
+            this.pickDateLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // appointmentID
+            // 
+            this.appointmentID.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.appointmentID.HeaderText = "AppointmentID";
+            this.appointmentID.MinimumWidth = 8;
+            this.appointmentID.Name = "appointmentID";
+            // 
+            // patientName
+            // 
+            this.patientName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.patientName.HeaderText = "PatientName";
+            this.patientName.MinimumWidth = 8;
+            this.patientName.Name = "patientName";
+            // 
+            // patientPhone
+            // 
+            this.patientPhone.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.patientPhone.HeaderText = "PatientPhone";
+            this.patientPhone.MinimumWidth = 6;
+            this.patientPhone.Name = "patientPhone";
+            // 
+            // filterButton
+            // 
+            this.filterButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(134)))), ((int)(((byte)(167)))), ((int)(((byte)(137)))));
+            this.filterButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.filterButton.FlatAppearance.BorderSize = 0;
+            this.filterButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.filterButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.filterButton.Location = new System.Drawing.Point(1316, 761);
+            this.filterButton.Name = "filterButton";
+            this.filterButton.Size = new System.Drawing.Size(136, 56);
+            this.filterButton.TabIndex = 64;
+            this.filterButton.Text = "Add";
+            this.filterButton.UseVisualStyleBackColor = false;
+            // 
+            // showAppointmentsOfTheDayForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1692, 844);
+            this.Controls.Add(this.filterButton);
+            this.Controls.Add(this.billingDataGridView);
+            this.Controls.Add(this.pickDateLabel);
+            this.Name = "showAppointmentsOfTheDayForm";
             this.Text = "showAppointmentsOfTheDayForm";
+            ((System.ComponentModel.ISupportInitialize)(this.billingDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.DataGridView billingDataGridView;
+        private System.Windows.Forms.Label pickDateLabel;
+        private System.Windows.Forms.DataGridViewTextBoxColumn appointmentID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn patientName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn patientPhone;
+        private System.Windows.Forms.Button filterButton;
     }
 }
