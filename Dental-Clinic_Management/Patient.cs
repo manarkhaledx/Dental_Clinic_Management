0a97f7790e63e51147c7a50b57a3668485e6779c
manar <96601859+manarkhaledx@users.noreply.github.com>
2023-12-18 22:59:08 +0200

    commit

--- Dental-Clinic_Management/Patient.cs
+++ Dental-Clinic_Management/Patient.cs
@@ -14,6 +14,7 @@ namespace Dental_Clinic_Management
     
     public partial class Patient
     {
+<<<<<<< HEAD
         public int patient_id { get; set; }
         public string Fname { get; set; }
         public string Lname { get; set; }
@@ -25,5 +26,21 @@ namespace Dental_Clinic_Management
         public virtual Appointment Appointment { get; set; }
         public virtual Payment Payment { get; set; }
         public virtual Prescription Prescription { get; set; }
+=======
+        public Patient()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+>>>>>>> parent of a06415a (commit)
     }
 }
