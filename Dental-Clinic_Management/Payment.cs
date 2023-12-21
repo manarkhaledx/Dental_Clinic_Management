1a97855d13c06d833e2c5f0a1dec526b1db0c682
manarkhaledx <manarkhaledx@outlook.com>
2023-12-21 23:24:54 +0200

    paymentDBModified

--- Dental-Clinic_Management/Payment.cs
+++ Dental-Clinic_Management/Payment.cs
@@ -15,6 +15,8 @@ namespace Dental_Clinic_Management
     {
         SqlCommand cmd;
         SqlDataReader dr;
+        private decimal totalCost;
+
         public Payment()
         {
             InitializeComponent();
@@ -25,68 +27,72 @@ namespace Dental_Clinic_Management
             con.ConnectionString = "data source = DESKTOP-8JPNOOB\\MSSQLSERVER01;database=Clinic;integrated security = true";
             return con;
         }
-
-        private void serviceListBox_SelectedIndexChanged(object sender, EventArgs e)
+        private void Payment_Load(object sender, EventArgs e)
         {
             try
             {
-                // Display selected service details in labels
-                if (serviceListBox.SelectedIndex != -1)
+                // Load service names into serviceListBox during form load
+                using (SqlConnection con = getConnection())
                 {
+                    con.Open();
+                    string query = "SELECT [ser_name] FROM [Clinic].[dbo].[dent_services]";
+                    cmd = new SqlCommand(query, con);
+                    dr = cmd.ExecuteReader();
+
+                    while (dr.Read())
+                    {
+                        serviceListBox.Items.Add(dr["ser_name"].ToString());
+                    }
+
+                    dr.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loading service names: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void serviceListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void addItemButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Add selected service name to serviceNameLabel
+                if (serviceListBox.SelectedItem != null)
+                {
+                    serviceNameLabel.Text += serviceListBox.SelectedItem.ToString() + Environment.NewLine;
+
+                    // Fetch and add the cost to totalCost
                     using (SqlConnection con = getConnection())
                     {
                         con.Open();
-                        string query = "SELECT [ser_id], [ser_name], [ser_price], [payment_id] FROM [Clinic].[dbo].[dent_services] WHERE [ser_name] = @serviceName";
+                        string query = "SELECT [ser_price] FROM [Clinic].[dbo].[dent_services] WHERE [ser_name] = @serviceName";
                         cmd = new SqlCommand(query, con);
                         cmd.Parameters.AddWithValue("@serviceName", serviceListBox.SelectedItem.ToString());
-                        dr = cmd.ExecuteReader();
 
-                        if (dr.Read())
+                        object result = cmd.ExecuteScalar();
+                        if (result != null)
                         {
-                            // Display service details in labels
-                            serviceNameLabel.Text = dr["ser_name"].ToString();
-                            serviceCostLabel.Text = dr["ser_price"].ToString();
+                            decimal serviceCost = Convert.ToDecimal(result);
+                            totalCost += serviceCost;
+                            serviceCostLabel.Text = "Total Cost: " + totalCost.ToString("C");
                         }
-
-                        dr.Close();
                     }
                 }
             }
             catch (Exception ex)
             {
-                MessageBox.Show("An error occurred: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show("Error adding item: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
+    
 
-        private void addItemButton_Click(object sender, EventArgs e)
-        {
-            // Clear existing items in the list box
-            serviceListBox.Items.Clear();
 
-            // Fetch data from the database and populate the list box
-            using (SqlConnection con = getConnection())
-            {
-                con.Open();
-                string query = "SELECT [ser_id], [ser_name], [ser_price], [payment_id] FROM [Clinic].[dbo].[dent_services]";
-                cmd = new SqlCommand(query, con);
-                dr = cmd.ExecuteReader();
 
-                while (dr.Read())
-                {
-                    // Display service name in the list box
-                    serviceListBox.Items.Add(dr["ser_name"].ToString());
-                }
-
-                dr.Close();
-            }
-        }
-
-        private void Payment_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void confirmButton_Click(object sender, EventArgs e)
+    private void confirmButton_Click(object sender, EventArgs e)
         {
 
         }
