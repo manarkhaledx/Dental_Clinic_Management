d1b6938d5216962964edd706c695b84ecd2b5fda
Yomna <monyhesham2002@gmail.com>
2023-12-21 11:44:52 -0800

    coding payment form

--- Dental-Clinic_Management/Payment.Designer.cs
+++ Dental-Clinic_Management/Payment.Designer.cs
@@ -30,15 +30,10 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Payment));
             this.confirmButton = new System.Windows.Forms.Button();
-            this.serviceCostTextBox = new System.Windows.Forms.TextBox();
-            this.serviceCostLabel = new System.Windows.Forms.Label();
-            this.serviceNameLabel = new System.Windows.Forms.Label();
-            this.addServiceTextBox = new System.Windows.Forms.TextBox();
             this.paymentMethodGroupBox = new System.Windows.Forms.GroupBox();
             this.cashRadioButton = new System.Windows.Forms.RadioButton();
             this.visaRadioButton = new System.Windows.Forms.RadioButton();
             this.serviceListBox = new System.Windows.Forms.ListBox();
-            this.addItemButton = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
             this.totalCostTextBox = new System.Windows.Forms.TextBox();
             this.totalCostLabel = new System.Windows.Forms.Label();
@@ -60,61 +55,13 @@
             this.confirmButton.FlatAppearance.BorderSize = 0;
             this.confirmButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.confirmButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.confirmButton.Location = new System.Drawing.Point(969, 618);
+            this.confirmButton.Location = new System.Drawing.Point(934, 604);
             this.confirmButton.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.confirmButton.Name = "confirmButton";
             this.confirmButton.Size = new System.Drawing.Size(123, 46);
             this.confirmButton.TabIndex = 77;
             this.confirmButton.Text = "Confirm Payment";
             this.confirmButton.UseVisualStyleBackColor = false;
-            // 
-            // serviceCostTextBox
-            // 
-            this.serviceCostTextBox.BackColor = System.Drawing.SystemColors.MenuBar;
-            this.serviceCostTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.serviceCostTextBox.Font = new System.Drawing.Font("Microsoft Uighur", 22.2F, System.Drawing.FontStyle.Bold);
-            this.serviceCostTextBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(9)))), ((int)(((byte)(111)))), ((int)(((byte)(24)))));
-            this.serviceCostTextBox.Location = new System.Drawing.Point(347, 373);
-            this.serviceCostTextBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.serviceCostTextBox.Name = "serviceCostTextBox";
-            this.serviceCostTextBox.Size = new System.Drawing.Size(212, 32);
-            this.serviceCostTextBox.TabIndex = 82;
-            // 
-            // serviceCostLabel
-            // 
-            this.serviceCostLabel.AutoSize = true;
-            this.serviceCostLabel.Font = new System.Drawing.Font("Microsoft Uighur", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.serviceCostLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(9)))), ((int)(((byte)(111)))), ((int)(((byte)(24)))));
-            this.serviceCostLabel.Location = new System.Drawing.Point(370, 310);
-            this.serviceCostLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.serviceCostLabel.Name = "serviceCostLabel";
-            this.serviceCostLabel.Size = new System.Drawing.Size(130, 37);
-            this.serviceCostLabel.TabIndex = 81;
-            this.serviceCostLabel.Text = "Service Cost:";
-            // 
-            // serviceNameLabel
-            // 
-            this.serviceNameLabel.AutoSize = true;
-            this.serviceNameLabel.Font = new System.Drawing.Font("Microsoft Uighur", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.serviceNameLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(9)))), ((int)(((byte)(111)))), ((int)(((byte)(24)))));
-            this.serviceNameLabel.Location = new System.Drawing.Point(370, 180);
-            this.serviceNameLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.serviceNameLabel.Name = "serviceNameLabel";
-            this.serviceNameLabel.Size = new System.Drawing.Size(142, 37);
-            this.serviceNameLabel.TabIndex = 80;
-            this.serviceNameLabel.Text = "Service Name:";
-            // 
-            // addServiceTextBox
-            // 
-            this.addServiceTextBox.BackColor = System.Drawing.SystemColors.MenuBar;
-            this.addServiceTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.addServiceTextBox.Font = new System.Drawing.Font("Microsoft Uighur", 22.2F, System.Drawing.FontStyle.Bold);
-            this.addServiceTextBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(9)))), ((int)(((byte)(111)))), ((int)(((byte)(24)))));
-            this.addServiceTextBox.Location = new System.Drawing.Point(347, 235);
-            this.addServiceTextBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.addServiceTextBox.Name = "addServiceTextBox";
-            this.addServiceTextBox.Size = new System.Drawing.Size(212, 32);
-            this.addServiceTextBox.TabIndex = 79;
             // 
             // paymentMethodGroupBox
             // 
@@ -158,26 +105,13 @@
             // serviceListBox
             // 
             this.serviceListBox.FormattingEnabled = true;
-            this.serviceListBox.Location = new System.Drawing.Point(595, 90);
-            this.serviceListBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.serviceListBox.Location = new System.Drawing.Point(516, 103);
+            this.serviceListBox.Margin = new System.Windows.Forms.Padding(2);
             this.serviceListBox.Name = "serviceListBox";
+            this.serviceListBox.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
             this.serviceListBox.Size = new System.Drawing.Size(460, 355);
             this.serviceListBox.TabIndex = 75;
-            // 
-            // addItemButton
-            // 
-            this.addItemButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(134)))), ((int)(((byte)(167)))), ((int)(((byte)(137)))));
-            this.addItemButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.addItemButton.FlatAppearance.BorderSize = 0;
-            this.addItemButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.addItemButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.addItemButton.Location = new System.Drawing.Point(377, 90);
-            this.addItemButton.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
-            this.addItemButton.Name = "addItemButton";
-            this.addItemButton.Size = new System.Drawing.Size(123, 46);
-            this.addItemButton.TabIndex = 76;
-            this.addItemButton.Text = "Add Item";
-            this.addItemButton.UseVisualStyleBackColor = false;
+            this.serviceListBox.SelectedIndexChanged += new System.EventHandler(this.serviceListBox_SelectedIndexChanged);
             // 
             // panel2
             // 
@@ -196,7 +130,7 @@
             this.totalCostTextBox.Font = new System.Drawing.Font("Microsoft Uighur", 22.2F, System.Drawing.FontStyle.Bold);
             this.totalCostTextBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(9)))), ((int)(((byte)(111)))), ((int)(((byte)(24)))));
             this.totalCostTextBox.Location = new System.Drawing.Point(629, 477);
-            this.totalCostTextBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.totalCostTextBox.Margin = new System.Windows.Forms.Padding(2);
             this.totalCostTextBox.Name = "totalCostTextBox";
             this.totalCostTextBox.Size = new System.Drawing.Size(212, 32);
             this.totalCostTextBox.TabIndex = 73;
@@ -225,7 +159,7 @@
             this.panel8.Location = new System.Drawing.Point(0, 0);
             this.panel8.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.panel8.Name = "panel8";
-            this.panel8.Size = new System.Drawing.Size(281, 487);
+            this.panel8.Size = new System.Drawing.Size(281, 670);
             this.panel8.TabIndex = 71;
             // 
             // paymentButton
@@ -316,20 +250,15 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(976, 487);
+            this.ClientSize = new System.Drawing.Size(1131, 670);
             this.Controls.Add(this.confirmButton);
-            this.Controls.Add(this.serviceCostTextBox);
-            this.Controls.Add(this.serviceCostLabel);
-            this.Controls.Add(this.serviceNameLabel);
-            this.Controls.Add(this.addServiceTextBox);
             this.Controls.Add(this.paymentMethodGroupBox);
             this.Controls.Add(this.serviceListBox);
-            this.Controls.Add(this.addItemButton);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.totalCostTextBox);
             this.Controls.Add(this.totalCostLabel);
             this.Controls.Add(this.panel8);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Payment";
             this.Text = "Payment";
             this.paymentMethodGroupBox.ResumeLayout(false);
@@ -344,15 +273,10 @@
         #endregion
 
         private System.Windows.Forms.Button confirmButton;
-        private System.Windows.Forms.TextBox serviceCostTextBox;
-        private System.Windows.Forms.Label serviceCostLabel;
-        private System.Windows.Forms.Label serviceNameLabel;
-        private System.Windows.Forms.TextBox addServiceTextBox;
         private System.Windows.Forms.GroupBox paymentMethodGroupBox;
         private System.Windows.Forms.RadioButton cashRadioButton;
         private System.Windows.Forms.RadioButton visaRadioButton;
         private System.Windows.Forms.ListBox serviceListBox;
-        private System.Windows.Forms.Button addItemButton;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.TextBox totalCostTextBox;
         private System.Windows.Forms.Label totalCostLabel;
--- Dental-Clinic_Management/Payment.cs
+++ Dental-Clinic_Management/Payment.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,34 @@ namespace Dental_Clinic_Management
         public Payment()
         {
             InitializeComponent();
+        }
+
+        private void serviceListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = "data source=DESKTOP-8JPNOOB\\MSSQLSERVER01;database=Clinic;integrated security=true";
+            conn.Open();
+
+            // Create a SqlCommand to retrieve data
+            SqlCommand retrieveCmd = new SqlCommand("SELECT ser_name, ser_price, FROM dent_services", conn);
+
+            // Use a SqlDataReader to read the data
+            using (SqlDataReader reader = retrieveCmd.ExecuteReader())
+            {
+                // Clear existing items in the ListBox
+                serviceListBox.Items.Clear();
+
+                // Read the data and add it to the ListBox
+                while (reader.Read())
+                {
+                    // Assuming you want to display a formatted string in the ListBox
+                    string listItem = $"{reader["ser_name"]} - {reader["ser_price"]}";
+                    serviceListBox.Items.Add(listItem);
+                }
+            }
+
+            // Don't forget to close the connection when done
+            conn.Close();
         }
     }
 }
