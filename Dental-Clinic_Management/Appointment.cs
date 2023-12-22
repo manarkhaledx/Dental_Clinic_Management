fcbac051ea777f79c9cdf7a335f5f222562fbed6
Abdelrhman-2002 <bodyegypt2016@gmail.com>
2023-12-23 01:07:41 +0200

    el lol el akher

--- Dental-Clinic_Management/Appointment.cs
+++ Dental-Clinic_Management/Appointment.cs
@@ -440,7 +440,7 @@ namespace Dental_Clinic_Management
         {
             try
             {
-                DateTime dateOfTheDay = billingHistoryDateTimePicker.Value;
+                DateTime dateOfTheDay = billingHistoryDateTimePicker.Value.Date;
                 appointmentDataBaseQueries.ShowAppointmentsInDataGridView(appointmentDataGridView, dateOfTheDay);
             }
             catch (Exception ex)
