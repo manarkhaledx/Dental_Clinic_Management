09da883c429fcdf134eecb4dc7c7f8683704d6d7
nourhan36 <nourhanibrahim2004@gmail.com>
2023-12-22 14:03:29 +0200

    add appontment edited

--- Dental-Clinic_Management/AddAppointment.cs
+++ Dental-Clinic_Management/AddAppointment.cs
@@ -15,7 +15,6 @@ namespace Dental_Clinic_Management
 {
     public partial class AddAppointment : Form
     {
-        SqlConnection conn; // Assuming that you have a SqlConnection object as a class-level field
 
         public AddAppointment()
         {
@@ -87,55 +86,6 @@ namespace Dental_Clinic_Management
                 MessageBox.Show("An error occurred: " + ex.Message);
             }
 
-            /*try
-            {
-                if (phoneTextBox.Text != string.Empty || appointmentDateTimePicker.Value != null || timeTextBox.Text != string.Empty)
-                {
-                    int patientId = GetPatientIdByPhoneNumber(phoneTextBox.Text);
-
-                    if (patientId != -1)
-                    {
-                        SqlConnection conn = new SqlConnection();
-                        conn.ConnectionString = "data source=DESKTOP-8JPNOOB\\MSSQLSERVER01;database=Clinic;integrated security=true";
-                        conn.Open();
-
-                        // Check if the patient exists before inserting the appointment
-                        if (IsPatientExists(patientId, conn))
-                        {
-                            SqlCommand cmd = new SqlCommand("INSERT INTO Appointment " +
-                                                           "(patient_id, pat_phone, app_time, app_date) " +
-                                                           "VALUES (@patient_id, @pat_phone, @app_time, @app_date)", conn);
-
-                            cmd.Parameters.AddWithValue("@patient_id", patientId);
-                            cmd.Parameters.AddWithValue("@pat_phone", phoneTextBox.Text);
-                            cmd.Parameters.AddWithValue("@app_time", timeTextBox.Text);
-                            cmd.Parameters.AddWithValue("@app_date", appointmentDateTimePicker.Value);
-
-                            cmd.ExecuteNonQuery();
-
-                            MessageBox.Show("Appointment saved successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                        }
-                        else
-                        {
-                            MessageBox.Show("Patient not found. Please check the phone number.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                        }
-
-                        conn.Close();
-                    }
-                    else
-                    {
-                        MessageBox.Show("Patient not found. Please check the phone number.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    }
-                }
-                else
-                {
-                    MessageBox.Show("Please enter a value in all fields.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }*/
         }
 
         private static int GetPatientIDByPhone(SqlConnection con, string phone)
@@ -169,40 +119,5 @@ namespace Dental_Clinic_Management
             }
         }
 
-        private int GetPatientIdByPhoneNumber(string phoneNumber)
-        {
-            int patientId = -1;
-
-            using (SqlConnection conn = new SqlConnection("data source=DESKTOP-8JPNOOB\\MSSQLSERVER01;database=Clinic;integrated security=true"))
-            {
-                conn.Open();
-
-                using (SqlCommand cmd = new SqlCommand("SELECT patient_id FROM Patient WHERE pat_phone = @pat_phone", conn))
-                {
-                    cmd.Parameters.AddWithValue("@pat_phone", phoneNumber);
-
-                    object result = cmd.ExecuteScalar();
-
-                    if (result != null && result != DBNull.Value)
-                    {
-                        patientId = Convert.ToInt32(result);
-                    }
-                }
-            }
-
-            return patientId;
-        }
-
-        private bool IsPatientExists(int patientId, SqlConnection conn)
-        {
-            using (SqlCommand cmd = new SqlCommand("SELECT COUNT(*) FROM Patient WHERE patient_id = @patient_id", conn))
-            {
-                cmd.Parameters.AddWithValue("@patient_id", patientId);
-
-                int count = (int)cmd.ExecuteScalar();
-
-                return count > 0;
-            }
-        }
     }
 }
