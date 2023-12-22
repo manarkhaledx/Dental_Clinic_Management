78a389ae8e1002cab0ec733a072bff5981e26c74
nourhan36 <nourhanibrahim2004@gmail.com>
2023-12-22 13:30:54 +0200

    add appointment DB

--- Dental-Clinic_Management/AddAppointment.cs
+++ Dental-Clinic_Management/AddAppointment.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
 using System.Xml.Linq;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
 
 namespace Dental_Clinic_Management
 {
@@ -33,6 +34,60 @@ namespace Dental_Clinic_Management
         private void saveNewAppointmentButton_Click(object sender, EventArgs e)
         {
             try
+            {
+                string phone = phoneTextBox.Text;
+                string time = timeTextBox.Text;
+                DateTime date = appointmentDateTimePicker.Value;
+                if (phoneTextBox.Text != string.Empty && appointmentDateTimePicker.Value != null && timeTextBox.Text != string.Empty)
+                {
+                    using (SqlConnection con = new SqlConnection("data source=DESKTOP-8JPNOOB\\MSSQLSERVER01;database=Clinic;integrated security=true"))
+                    {
+                        con.Open();
+
+                        // Retrieve the patientID based on the provided phone number
+                        int patientID = GetPatientIDByPhone(con, phoneTextBox.Text);
+
+                        // Check if patientID is valid (greater than 0) before proceeding
+                        if (patientID > 0)
+                        {
+                            // Insert the new appointment using the retrieved patientID
+                            using (SqlCommand cmd = new SqlCommand("INSERT INTO appointment (pat_phone, app_time, app_date, patientID) VALUES (@phone, @time, @date, @patientID)", con))
+                            {
+                                cmd.Parameters.AddWithValue("@phone", phone);
+                                cmd.Parameters.AddWithValue("@time", time);
+                                cmd.Parameters.AddWithValue("@date", date);
+                                cmd.Parameters.AddWithValue("@patientID", patientID);
+
+                                int rowsAffected = cmd.ExecuteNonQuery();
+
+                                if (rowsAffected > 0)
+                                {
+                                    MessageBox.Show("Appointment added successfully.");
+                                }
+                                else
+                                {
+                                    MessageBox.Show("Failed to add appointment.");
+                                }
+                            }
+                        }
+                        else
+                        {
+                            // Handle the case where the patientID could not be found
+                            MessageBox.Show("Patient not found for the provided phone number.");
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please enter valid values for phone, time, and date.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message);
+            }
+
+            /*try
             {
                 if (phoneTextBox.Text != string.Empty || appointmentDateTimePicker.Value != null || timeTextBox.Text != string.Empty)
                 {
@@ -80,6 +135,37 @@ namespace Dental_Clinic_Management
             catch (Exception ex)
             {
                 MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }*/
+        }
+
+        private static int GetPatientIDByPhone(SqlConnection con, string phone)
+        {
+            try
+            {
+                using (SqlCommand cmd = new SqlCommand("SELECT pat_id FROM patient WHERE pat_phone=@phone", con))
+                {
+                    cmd.Parameters.AddWithValue("@phone", phone);
+
+                    // ExecuteScalar is used to retrieve a single value from the query result
+                    object result = cmd.ExecuteScalar();
+
+                    // Check if a result was found
+                    if (result != null)
+                    {
+                        // Convert the result to an integer (patientID)
+                        return Convert.ToInt32(result);
+                    }
+                    else
+                    {
+                        // Return 0 if no matching patientID was found
+                        return 0;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message);
+                return 0;
             }
         }
 
--- Dental-Clinic_Management/showAppointmentsOfTheDayForm.cs
+++ Dental-Clinic_Management/showAppointmentsOfTheDayForm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,41 @@ namespace Dental_Clinic_Management
 {
     public partial class showAppointmentsOfTheDayForm : Form
     {
+        SqlCommand cmd;
+        SqlDataReader dr;
+        protected static SqlConnection getConnection()
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = "data source = DESKTOP-8JPNOOB\\MSSQLSERVER01;database=Clinic;integrated security=true";
+            return con;
+        }
         public showAppointmentsOfTheDayForm()
         {
             InitializeComponent();
+        }
+
+        public static class appointmentDataBaseQueries
+        {
+            public static void addappointment(string phone, string time, DateTime date, int patientID)
+            {
+                using (SqlConnection con = getConnection())
+                {
+                    con.Open();
+
+                    using (SqlCommand cmd = new SqlCommand("SELECT * FROM appointment WHERE pat_phone=@phone AND app_time=@time AND app_date=@date", con))
+                    {
+                        cmd.Parameters.AddWithValue("pat_phone", phone);
+                        cmd.Parameters.AddWithValue("app_time", time);
+                        cmd.Parameters.AddWithValue("app_date", date);
+
+                        using (SqlDataReader dr = cmd.ExecuteReader())
+                        {
+                            
+                            
+                        }
+                    }
+                }
+            }
         }
     }
 }
