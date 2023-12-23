122600abf751aa3efed8562b5fae22d953a0e5ab
Abdelrhman-2002 <bodyegypt2016@gmail.com>
2023-12-23 22:50:31 +0200

    edit settings

--- Dental-Clinic_Management/settings.cs
+++ Dental-Clinic_Management/settings.cs
@@ -29,49 +29,63 @@ namespace Dental_Clinic_Management
         private void saveAccountButton_Click(object sender, EventArgs e)
         {
             // Get values from textboxes
-            string currentPassword = passwordTextBox.Text;
+            string currentUsername = passwordTextBox.Text;
             string newUsername = newUsernameTextBox.Text;
             string newPassword = newPasswordTextBox.Text;
 
-            // Validate current password (you may need to adjust this part based on your authentication mechanism)
-            if (!IsValidCurrentPassword(currentPassword))
-            {
-                MessageBox.Show("Invalid current password. Please try again.");
-                return;
-            }
-
             // Update username and password in the Dentist table
-            UpdateDentistCredentials(newUsername, newPassword);
-
-            MessageBox.Show("Account information updated successfully.");
+            UpdateDentistCredentials(currentUsername, newUsername, newPassword);
         }
 
-        private bool IsValidCurrentPassword(string currentPassword)
-        {
-            // Implement your logic to validate the current password
-            // You may need to query the database to check if the current password is correct
-            // For security reasons, consider using hashing for password storage and comparison
-            // Return true if the current password is valid, otherwise return false
-            // Example: return CheckIfCurrentPasswordIsValid(currentPassword);
-            return true; // Replace this with your actual validation logic
-        }
-
-        private void UpdateDentistCredentials(string newUsername, string newPassword)
+        private void UpdateDentistCredentials(string currentUsername, string newUsername, string newPassword)
         {
             using (SqlConnection con = getConnection())
             {
                 con.Open();
-                string updateQuery = "UPDATE Dentist SET username = @newUsername, pass = @newPassword WHERE /* Add your condition, e.g., dentist_id = @dentistId */";
+
+                // Retrieve the dentist ID based on the current username
+                int dentistId = GetDentistIdByUsername(con, currentUsername);
+
+                if (dentistId == -1)
+                {
+                    MessageBox.Show("Dentist not found. Update failed.");
+                    return;
+                }
+
+                string updateQuery = "UPDATE Dentist SET username = @newUsername, pass = @newPassword WHERE dentist_id = @dentistId";
 
                 using (SqlCommand cmd = new SqlCommand(updateQuery, con))
                 {
                     cmd.Parameters.AddWithValue("@newUsername", newUsername);
                     cmd.Parameters.AddWithValue("@newPassword", newPassword);
-                    // Add condition parameter, e.g., cmd.Parameters.AddWithValue("@dentistId", dentistId);
+                    cmd.Parameters.AddWithValue("@dentistId", dentistId);
 
                     cmd.ExecuteNonQuery();
                 }
+
+                MessageBox.Show("Account information updated successfully.");
             }
+        }
+
+        private int GetDentistIdByUsername(SqlConnection con, string currentUsername)
+        {
+            string selectQuery = "SELECT dentist_id FROM Dentist WHERE username = @currentUsername";
+            int dentistId = -1;
+
+            using (SqlCommand cmd = new SqlCommand(selectQuery, con))
+            {
+                cmd.Parameters.AddWithValue("@currentUsername", currentUsername);
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        dentistId = reader.GetInt32(0);
+                    }
+                }
+            }
+
+            return dentistId;
         }
     }
 }
