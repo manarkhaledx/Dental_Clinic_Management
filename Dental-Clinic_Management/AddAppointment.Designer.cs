629b6f7dca80b6e4a92182653861166df9d77d1a
Mark Mamdouh <mark.mamdouh67@yahoo.com>
2023-12-23 02:32:50 +0200

    add (addAppointmentForm gui)

--- Dental-Clinic_Management/AddAppointment.Designer.cs
+++ Dental-Clinic_Management/AddAppointment.Designer.cs
@@ -44,42 +44,44 @@
             // 
             this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(97)))), ((int)(((byte)(130)))), ((int)(((byte)(100)))));
             this.panel3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.panel3.Location = new System.Drawing.Point(90, 386);
-            this.panel3.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.panel3.Location = new System.Drawing.Point(177, 272);
+            this.panel3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(628, 2);
-            this.panel3.TabIndex = 56;
+            this.panel3.Size = new System.Drawing.Size(559, 1);
+            this.panel3.TabIndex = 65;
             // 
             // phoneTextBox
             // 
             this.phoneTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.phoneTextBox.Font = new System.Drawing.Font("Microsoft Uighur", 22.2F, System.Drawing.FontStyle.Bold);
             this.phoneTextBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(9)))), ((int)(((byte)(111)))), ((int)(((byte)(24)))));
-            this.phoneTextBox.Location = new System.Drawing.Point(87, 329);
+            this.phoneTextBox.Location = new System.Drawing.Point(174, 226);
+            this.phoneTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.phoneTextBox.Name = "phoneTextBox";
-            this.phoneTextBox.Size = new System.Drawing.Size(628, 48);
-            this.phoneTextBox.TabIndex = 54;
+            this.phoneTextBox.Size = new System.Drawing.Size(559, 40);
+            this.phoneTextBox.TabIndex = 64;
             this.phoneTextBox.Text = "Phone Number";
             // 
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(97)))), ((int)(((byte)(130)))), ((int)(((byte)(100)))));
             this.panel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.panel2.Location = new System.Drawing.Point(87, 480);
-            this.panel2.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.panel2.Location = new System.Drawing.Point(174, 347);
+            this.panel2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(628, 2);
-            this.panel2.TabIndex = 53;
+            this.panel2.Size = new System.Drawing.Size(559, 1);
+            this.panel2.TabIndex = 63;
             // 
             // timeTextBox
             // 
             this.timeTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.timeTextBox.Font = new System.Drawing.Font("Microsoft Uighur", 22.2F, System.Drawing.FontStyle.Bold);
             this.timeTextBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(9)))), ((int)(((byte)(111)))), ((int)(((byte)(24)))));
-            this.timeTextBox.Location = new System.Drawing.Point(87, 423);
+            this.timeTextBox.Location = new System.Drawing.Point(174, 301);
+            this.timeTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.timeTextBox.Name = "timeTextBox";
-            this.timeTextBox.Size = new System.Drawing.Size(628, 48);
-            this.timeTextBox.TabIndex = 52;
+            this.timeTextBox.Size = new System.Drawing.Size(559, 40);
+            this.timeTextBox.TabIndex = 62;
             this.timeTextBox.Text = "Time";
             // 
             // closePictureBox
@@ -87,10 +89,11 @@
             this.closePictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.closePictureBox.BackgroundImage = global::Dental_Clinic_Management.Properties.Resources.Exit;
             this.closePictureBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.closePictureBox.Location = new System.Drawing.Point(90, 117);
+            this.closePictureBox.Location = new System.Drawing.Point(917, 11);
+            this.closePictureBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.closePictureBox.Name = "closePictureBox";
-            this.closePictureBox.Size = new System.Drawing.Size(36, 40);
-            this.closePictureBox.TabIndex = 51;
+            this.closePictureBox.Size = new System.Drawing.Size(32, 32);
+            this.closePictureBox.TabIndex = 61;
             this.closePictureBox.TabStop = false;
             // 
             // saveNewAppointmentButton
@@ -99,10 +102,11 @@
             this.saveNewAppointmentButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.saveNewAppointmentButton.FlatAppearance.BorderSize = 0;
             this.saveNewAppointmentButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.saveNewAppointmentButton.Location = new System.Drawing.Point(615, 663);
+            this.saveNewAppointmentButton.Location = new System.Drawing.Point(644, 493);
+            this.saveNewAppointmentButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.saveNewAppointmentButton.Name = "saveNewAppointmentButton";
-            this.saveNewAppointmentButton.Size = new System.Drawing.Size(153, 71);
-            this.saveNewAppointmentButton.TabIndex = 50;
+            this.saveNewAppointmentButton.Size = new System.Drawing.Size(136, 57);
+            this.saveNewAppointmentButton.TabIndex = 60;
             this.saveNewAppointmentButton.Text = "Save";
             this.saveNewAppointmentButton.UseVisualStyleBackColor = false;
             // 
@@ -112,37 +116,38 @@
             this.appointmentDateTimePicker.CalendarTitleBackColor = System.Drawing.SystemColors.ControlText;
             this.appointmentDateTimePicker.CalendarTitleForeColor = System.Drawing.SystemColors.ControlText;
             this.appointmentDateTimePicker.Font = new System.Drawing.Font("Microsoft Uighur", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.appointmentDateTimePicker.Location = new System.Drawing.Point(231, 550);
+            this.appointmentDateTimePicker.Location = new System.Drawing.Point(302, 404);
+            this.appointmentDateTimePicker.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.appointmentDateTimePicker.Name = "appointmentDateTimePicker";
-            this.appointmentDateTimePicker.Size = new System.Drawing.Size(486, 50);
-            this.appointmentDateTimePicker.TabIndex = 49;
+            this.appointmentDateTimePicker.Size = new System.Drawing.Size(432, 43);
+            this.appointmentDateTimePicker.TabIndex = 59;
             // 
             // dateLabel
             // 
             this.dateLabel.AutoSize = true;
             this.dateLabel.Font = new System.Drawing.Font("Microsoft Uighur", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.dateLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(9)))), ((int)(((byte)(111)))), ((int)(((byte)(24)))));
-            this.dateLabel.Location = new System.Drawing.Point(80, 550);
+            this.dateLabel.Location = new System.Drawing.Point(168, 404);
             this.dateLabel.Name = "dateLabel";
-            this.dateLabel.Size = new System.Drawing.Size(87, 54);
-            this.dateLabel.TabIndex = 48;
+            this.dateLabel.Size = new System.Drawing.Size(73, 45);
+            this.dateLabel.TabIndex = 58;
             this.dateLabel.Text = "Date";
             // 
             // addAppointmentLabel
             // 
             this.addAppointmentLabel.AutoSize = true;
             this.addAppointmentLabel.Font = new System.Drawing.Font("Times New Roman", 28.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.addAppointmentLabel.Location = new System.Drawing.Point(64, 160);
+            this.addAppointmentLabel.Location = new System.Drawing.Point(154, 91);
             this.addAppointmentLabel.Name = "addAppointmentLabel";
-            this.addAppointmentLabel.Size = new System.Drawing.Size(444, 65);
-            this.addAppointmentLabel.TabIndex = 47;
+            this.addAppointmentLabel.Size = new System.Drawing.Size(363, 52);
+            this.addAppointmentLabel.TabIndex = 57;
             this.addAppointmentLabel.Text = "Add Appointment";
             // 
             // AddAppointment
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1099, 843);
+            this.ClientSize = new System.Drawing.Size(977, 599);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.phoneTextBox);
             this.Controls.Add(this.panel2);
@@ -152,6 +157,7 @@
             this.Controls.Add(this.appointmentDateTimePicker);
             this.Controls.Add(this.dateLabel);
             this.Controls.Add(this.addAppointmentLabel);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "AddAppointment";
             this.Text = "AddAppointment";
             ((System.ComponentModel.ISupportInitialize)(this.closePictureBox)).EndInit();
@@ -161,6 +167,7 @@
         }
 
         #endregion
+
         private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.TextBox phoneTextBox;
         private System.Windows.Forms.Panel panel2;
